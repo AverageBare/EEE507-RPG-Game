dac84e11f625f0024ff9cf666f23deda5ff52fa9
Jeremy Eastwood-Smith <47756230+AverageBare@users.noreply.github.com>
2019-03-03 13:36:13 +0000

    Game.cpp
    Create .cpp file for class functions

---
+++ Game.cpp
@@ -0,0 +1,25 @@
+/* 
+
+*/
+
+#include ('Game.h')
+
+
+void Game::MainMenu()
+{
+    std::cout << "MAIN MENU" << std::endl;
+    std::cout << "1. Create a new character" << std::endl;
+    std::cout << "0. Quit" << std::endl << std::endl;
+    std::cin >> option;
+
+}
+
+void Game::Update()
+{
+
+}
+
+void Game::Clean()
+{
+   // delete Game;
+}
