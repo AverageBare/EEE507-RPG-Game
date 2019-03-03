7b2cbf6075defdd9de8596864914de216d13ca92
Jeremy Eastwood-Smith <47756230+AverageBare@users.noreply.github.com>
2019-03-03 13:33:29 +0000

    Update game.h
    Header files can be loaded repeatedly, only prototype code, create functions in .cpp files as they are only loaded once; saving time and stack size

--- game.h
+++ game.h
@@ -40,23 +40,4 @@ public:
 
 };
 
-void Game::MainMenu()
-{
-    std::cout << "MAIN MENU" << std::endl;
-    std::cout << "1. Create a new character" << std::endl;
-    std::cout << "0. Quit" << std::endl << std::endl;
-    std::cin >> option;
-
-}
-
-void Game::Update()
-{
-
-}
-
-void Game::Clean()
-{
-   // delete Game;
-}
-
 #endif // GAME_H
