98c313001b6e07647a70f9ee79e2a87d7312391e
Conan420 <47893768+Conan420@users.noreply.github.com>
2019-04-18 23:26:00 +0100

    Update Item.cpp

--- Item.cpp
+++ Item.cpp
@@ -1,33 +1,151 @@
 #include "Item.h"
 
 
-Item::Item(void)
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+
+Item::Item()
 {
 
 }
+
+
+Item::Item(string loot, int ID, int value)
+{
+	loot = " ";
+	ID = 0; 
+	value = 0;
+}
+
+
+
 
 
 Item::~Item(void)
 {
 
+
 }
 
 
 
 
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
 
-
-
-Shotgun::Shotgun(void)
+Weapon::Weapon()
 {
+	WeaponDamage = 0;
+}
+
+int Weapon::GetDamage()
+{
+	return WeaponDamage;
+}
+
+
+Weapon::~Weapon()
+{
+
+}
+
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+
+Sword::Sword()
+{
+	this->loot = "Atlantean Sword";
+	ID = 1;
+	value = 50;
+	WeaponDamage = 2;
+
+}
+
+
+string Sword::GetLoot()
+{
+	return this->loot;
+
+	cout << "Slash" << endl;
+}
+
+int Sword::GetID()
+{
+	return this->ID;
+}
+
+int Sword::GetValue()
+{
+	return this->value;
+}
+
+int Sword::GetDamage()
+{
+	return WeaponDamage;
+
+}
+
+Sword::~Sword(void)
+{
+
+
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+
+Pistol::Pistol()
+{
+	this->loot = "44 Magnum";
+	ID = 2;
+	WeaponDamage = 4;
+
+}
+
+Pistol::~Pistol(void)
+{
+
+}
+
+
+// Functions
+
+string Pistol::GetLoot()
+{
+	return this->loot;
+}
+
+int Pistol::GetID()
+{
+	return this->ID;
+}
+
+int Pistol::GetValue()
+{
+	return this->value;
+}
+
+int Pistol::GetDamage()
+{
+	return WeaponDamage;
+
+}
+
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+
+Shotgun::Shotgun()
+{
+	this->loot = "Master Blaster";
+	ID = 3;
+	WeaponDamage = 5;
 	
 }
-
-
-void Fire()
-	{
-            std::cout << "Pow" << std::endl;
-    }
 
 
 Shotgun::~Shotgun(void)
@@ -35,14 +153,27 @@ Shotgun::~Shotgun(void)
 
 }
 
+// Functions
 
-Weapon::Weapon(void)
+string Shotgun::GetLoot()
 {
-
+	return this->loot;
 }
 
-
-Weapon::~Weapon(void)
+int Shotgun::GetID()
 {
-
+	return this->ID;
 }
+
+int Shotgun::GetValue()
+{
+	return this->value;
+}
+
+int Shotgun::GetDamage()
+{
+	return WeaponDamage;
+}
+
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
