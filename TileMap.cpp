24b41473d91e1d834dc10bb60217e098aa61d2fc
IAmGrootO-G <47866446+IAmGrootO-G@users.noreply.github.com>
2019-04-19 20:24:01 +0100

    The working version on just Printed Grid

--- TileMap.cpp
+++ TileMap.cpp
@@ -1,14 +1,38 @@
-//@author Theo De Groot
-//@licence
-//@copywrite pending.
+/** 
+	This is the map design code, used to create 
+	a grey open box with internal obstacles within which the game will be played
 
-/** This is the map design code, used to create 
-a grey open box with internal obstacles within which the game will be played
+	
+	@author Theo De Groot B00693240 <De_Groot-T@ulster.ac.uk>
+	@licence <https://github.com/AverageBare/EEE507-RPG-Game/blob/master/MIT%20Copyright%20License>
+	MIT License
+
+	Copyright (c) [2019] [If(!Broken{don'tFixIt();}]
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
 */
 
-// TileMap.H contains my variables and functions
 #include "TileMap.h"
 
+/**
+	Sets the cursor on the compile screen to the location X, Y 
+*/
 void gotoxy(int x, int y)
 {
 
@@ -20,97 +44,119 @@ void gotoxy(int x, int y)
 
 };
 
-//Constructor for the TileMap Class
+/**
+	Constructor for the TileMap Class
+*/
 TileMap::TileMap()
 {
-		game_over = false;
+	game_over=true;
+	console_handle=GetStdHandle( STD_OUTPUT_HANDLE );  // getting console handle
 	
 };
 
-
-bool game=true; // bool function to intialise the start of the game, for game = true
-
-
-
-
-bool Print_Grid() //function to print Grid to screen, also includes placeholders for items or characters. 
+/** 
+	Destructor
+*/
+TileMap::~TileMap()
 {
-	int x  = 1;
-	int y  = 1;
-	int x1 = 22;
-	int y1 = 1;
-	int x2 = 19;
-	int y2 = 9;
-	int x3 = 37;
-	int y3 = 18;
+	game_over = false;
+}
+
+/** 
+	bool function to intialise the start of the game, for game = true.
+*/
+bool game=true;
+
+
+/**
+	function to print Grid to screen. 
+	Design of Grid using "#" for walls and " " for the areas for players to walk. 
+*/
+bool TileMap::Print() 
+{
 	int player = 2;
-	int Enemy1 = 4;
-	int Enemy2 = 6;
-	int Enemy3 = 5;
-	int item1  = 0;
-	char w=219;			//Color grey
-	
-// Design of Grid using "#" for walls and "." for the areas for players to walk. 
-//Grid is 500 by 500 but actual size used is smaller to allow for alterations and expansion if required.
-char Grid[500][500] = {
+	int x = 1;
+	int y = 1;
+	// Temporary Grid to print onto the Grid specified in the class 
+	char temp_grid[100][100] = {
 	
 	"######################################################",
-	"#....................................................#",
-	"#..#######################..#######################..#",
-	"#..#                     #..#                     #..#",
-	"#..#######################..#######################..#",
-	"#....................................................#",
-	"#....................................................#",
-	"#..####..####..########################..####..####..#",
-	"#..#  #..#  #..#                      #..#  #..#  #..#",
-	"#..#  #..#  #..#                      #..#  #..#  #..#",
-	"#..#  #..#  #..#                      #..#  #..#  #..#",
-	"#..#  #..#  #..#                      #..#  #..#  #..#",
-	"#..#  #..#  #..#                      #..#  #..#  #..#",
-	"#..#  #..#  #..#                      #..#  #..#  #..#",
-	"#..####..####..########################..####..####..#",
-	"#....................................................#",
-	"#....................................................#",
-	"#..#######################..######################...#",
-	"#..#                     #..#                    #...#",
-	"#..#######################..######################...#",
-	"#....................................................#",
+	"#                                                    #",
+	"#  ######################    ######################  #",
+	"#  #                    #    #                    #  #",
+	"#  ######################    ######################  #",
+	"#                                                    #",
+	"#                                                    #",
+	"#  ####      ###########      ###########      ####  #",
+	"#  #  #      #     #####      #####     #      #  #  #",
+	"#  #  #      #     #              #     #      #  #  #",
+	"#  #  #      #     #              #     #      #  #  #",
+	"#  #  #      #     #              #     #      #  #  #",
+	"#  #  #      #     #              #     #      #  #  #",
+	"#  #  #      #     #####      #####     #      #  #  #",
+	"#  ####      ###########      ###########      ####  #",
+	"#                                                    #",
+	"#                                                    #",
+	"#  ######################    #####################   #",
+	"#  #                    #    #                   #   #",
+	"#  ######################    #####################   #",
+	"#                                                    #",
 	"######################################################",
 	
 	};
 
-int i;
-	while(game==true)
+	// Copy the temp grid into the object Grid
+	// Replace # symbols with white blocks
+	for(int k=0;k<=22;k++)
 	{
-
-		Sleep(100);
-		system("cls");
-		for (i=0;i<22;i++)
-		{	
-	
-			cout<<Grid[i]<<endl;
-			
-		}
-
-
-		for(int k=0;k<=22;k++)
 		for(int j=0;j<=54;j++)
 		{
-			if(Grid[k][j]=='#')
-				Grid[k][j]=219;
+			// If it's a # make it a white block
+			if(temp_grid[k][j]=='#') Grid[k][j]=219;
+			// otherwise, just whatever it was in the temp grid
+			else Grid[k][j] = temp_grid[k][j];
 		}
+	}
+
+
+	while(game==true)
+	{
+		Sleep(100);
+		system("cls");
+
+		// Paint the grid from the object
+		for(int i=0; i<=22; i++)
+		{
+			for(int j=0; j <=54; j++)
+			{
+				cout << Grid[i][j];
+			}
+			cout << endl;
+		}
+	/** 
+		While the game is true the 'For loop' checks for all values of I and J that are
+		"#" and replaces that symbol with a white square
+	*/
 		cout<<"Inventory";
 		
-		Grid[y][x]=player;
+		Grid[y][x] = player; 
 	}
 	return (0);
 }
-
-int main()
-{
-	Print_Grid();
-	cout << Print_Grid; 
-
-	return (0);
-}
-
+/**
+	This gets the information from the CCreator array to be printed on the Map screen
+*/
+//void TileMap::PlayerPlacer()
+//{
+//	int size;
+//	char EnemyGrid[100][100];
+//	size = Players.NumPlayers();
+//
+//	//Pointer to the array of "Enemy" Characters
+//	Character * enemies = Players.EnemyArray();
+//	int loop;
+//	for(loop=0; loop < size; loop++);
+//	{
+//		cout << enemies[loop].getIcon() << endl;
+//	};
+//}
--- TileMap.h
+++ TileMap.h
@@ -1,37 +1,80 @@
-//@author Theo De Groot
-//@licence
-//@copywrite pending.
+/** 
+	This is the map design code, used to create 
+	a grey open box with internal obstacles within which the game will be played
 
-/** This is the map design code, used to create 
-a grey open box with internal obstacles within which the game will be played
+	
+	@author Theo De Groot B00693240 <De_Groot-T@ulster.ac.uk>
+	@licence <https://github.com/AverageBare/EEE507-RPG-Game/blob/master/MIT%20Copyright%20License>
+	MIT License
+
+	Copyright (c) [2019] [If(!Broken{don'tFixIt();}]
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
 */
 
+
+/** 
+	This is the map design code, used to create 
+	a grey open box with internal obstacles within which the game will be played
+*/
+
+//Standard Includes.
 #pragma once
-
 #include <iostream>
+#include <conio.h>
+#include <string.h>
+#include <Windows.h>
+#include <stdlib.h>
+#include <fstream>
 
-#include <conio.h> 
-//enables compilers to provide console input/output.
-
-#include <Windows.h> 
-//contains declarations for all of the functions in the Windows API,
-//all the common macros used by Windows programmers, 
-//and all the data types used by the various functions and subsystems.
-#include "Inventory.h"
-#include "Item.h"
-
+//Custom Includes.
+#include "Character.h"
+#include "CharCreator.h"
 using namespace std;
 
 class TileMap 
 {
 
+// Private Data
 private:
 	bool game_over;
-	bool Print_Grid(char Grid);
-	char Grid[500][500];
-	
 
 public:
+	//Constructor & Destructor.
 	TileMap();
-	 
+	~TileMap();
+
+	//Service Functions.
+	bool TileMap::Print();
+	void PlayerPlacer();
+
+
+	HANDLE console_handle;	// handle for the console      
+    COORD cur_cord;			// COORD struct to keep the coordinate
+	CCreator Players();
+	
+	//Variables(Global)
+	char Grid[100][100];
+	int x;
+	int y;
+	int player; 
+	int i;
+
+
 };
