30aacb4ae8bdc5800455081aadfd337a40f7b84d
Conan420 <47893768+Conan420@users.noreply.github.com>
2019-04-19 22:39:45 +0100

    Update Item.cpp

--- Item.cpp
+++ Item.cpp
@@ -1,8 +1,9 @@
+
 #include "Item.h"
 
 
 //////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////
+//////////////////////////// ITEM ///////////////////////////////////////
 
 /** Default constructor of class Item
 */
@@ -25,9 +26,9 @@ Item::Item(string loot, int ID, int value)
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
 
-/** Default constructor for class Weapon which initializes the values of WeaponDamage and
-*   WeaponRange as = 0.
-*
+/** Default constructor for derived class Weapon which initializes the variables WeaponDamage and WeaponRange.
+*   The protected variables that will be inhereted by derived weapon classes.
+*   These variables have been declared and initialiased as = 0;
 */ 
 Weapon::Weapon()
 {
@@ -35,11 +36,18 @@ Weapon::Weapon()
 	WeaponRange = 0;
 }
 
+// ACCESSORS 
+
+/** Accessor for class Weapon in order to publicy access the protected variable WeaponDamage.
+*   @return this WeaponDamage
+*/
 int Weapon::GetDamage()
 {
 	return WeaponDamage;
 }
-
+/** Accessor for class Weapon in order to publicy access the protected variable WeaponRange.
+*   @return this WeaponRange
+*/
 int Weapon::GetRange()
 {
 	return WeaponRange;
@@ -54,6 +62,10 @@ Weapon::~Weapon()
 //////////////////////////////////////////////////////////////////////////
 
 
+/** Default constructor for class Sword derived from base class Weapon, when an object is created from class Sword 
+*   it will be initialised with this list of inhereted protected variables that the author has allocated. To publicy
+*   access these protected variables other authors may use the Accessors below.
+*/
 Sword::Sword()
 {
 	this->loot = "Atlantean Sword";
@@ -64,29 +76,45 @@ Sword::Sword()
 
 }
 
+// ACCESSORS
 
+/** Accessor for class Sword in order to publicy access the protected string loot.
+*   @return this Swords loot(name)
+*/
 string Sword::GetLoot()
 {
 	return this->loot;
 
 }
 
+/** Accessor for class Sword in order to publicy access the protected variable ID.
+*   @return this Swords ID(Tag)
+*/
 int Sword::GetID()
 {
 	return this->ID;
 }
 
+/** Accessor for class Sword in order to publicy access the protected variable value.
+*   @return this Swords value.
+*/
 int Sword::GetValue()
 {
 	return this->value;
 }
 
+/** Accessor for class Sword in order to publicy access the protected variable saved as WeaponDamage.
+*   @return this Swords WeaponDamage.
+*/
 int Sword::GetDamage()
 {
 	return this->WeaponDamage;
 
 }
 
+/** Accessor for class Sword in order to publicy access the protected variable saved as WeaponRange.
+*   @return this Swords WeaponRange.
+*/
 int Sword::GetRange()
 {
 	return this->WeaponRange;
@@ -102,7 +130,10 @@ Sword::~Sword(void)
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
 
-
+/** Default constructor for class Pistol derived from base class Weapon, when an object is created from class Pistol 
+*   it will be initialised with this list of inhereted protected variables that the author has allocated. To publicy
+*   access these protected variables other authors may use the Accessors below.
+*/
 Pistol::Pistol()
 {
 	this->loot = "44 Magnum";
@@ -119,29 +150,44 @@ Pistol::~Pistol(void)
 }
 
 
-// Functions
+// ACCESSORS
 
+/** Accessor for class Pistol in order to publicy access the protected string saved as loot.
+*   @return this Pistols loot(name)
+*/
 string Pistol::GetLoot()
 {
 	return this->loot;
 }
 
+/** Accessor for class Pistol in order to publicy access the protected variable saved as ID.
+*   @return this Pistols ID.
+*/
 int Pistol::GetID()
 {
 	return this->ID;
 }
 
+/** Accessor for class Pistol in order to publicy access the protected variable saved as value.
+*   @return this Pistols value.
+*/
 int Pistol::GetValue()
 {
 	return this->value;
 }
 
+/** Accessor for class Pistol in order to publicy access the protected variable saved as WeaponDamage.
+*   @return this Pistols WeaponDamage.
+*/
 int Pistol::GetDamage()
 {
 	return this->WeaponDamage;
 
 }
 
+/** Accessor for class Pistol in order to publicy access the protected variable save as WeaponRange.
+*   @return this Pistols WeaponRange.
+*/
 int Pistol::GetRange()
 {
 	return this->WeaponRange;
@@ -151,7 +197,10 @@ int Pistol::GetRange()
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
 
-
+/** Default constructor for class Shotgun derived from base class Weapon, when an object is created from class Shotgun 
+*   it will be initialised with this list of inhereted protected variables that the author has allocated. To publicy
+*   access these protected variables other authors may use the Accessors below.
+*/
 Shotgun::Shotgun()
 {
 	this->loot = "Master Blaster";
@@ -162,39 +211,100 @@ Shotgun::Shotgun()
 	
 }
 
-
 Shotgun::~Shotgun(void)
 {
 
 }
 
-// Functions
+// ACCESSORS
 
+/** Accessor for class Shotgun in order to publicy access the protected string saved as loot.
+*   @return this Shotguns loot(name).
+*/
 string Shotgun::GetLoot()
 {
 	return this->loot;
 }
 
+/** Accessor for class Shotgun in order to publicy access the protected variable saved as ID.
+*   @return this Shotguns ID.
+*/
 int Shotgun::GetID()
 {
 	return this->ID;
 }
 
+/** Accessor for class Shotgun in order to publicy access the protected variable saved as value.
+*   @return this Shotguns value.
+*/
 int Shotgun::GetValue()
 {
 	return this->value;
 }
 
+/** Accessor for class Shotgun in order to publicy access the protected variable saved as WeaponDamage.
+*   @return this Shotguns WeaponDamage.
+*/
 int Shotgun::GetDamage()
 {
 	return this->WeaponDamage;
 }
 
+/** Accessor for class Shotgun in order to publicy access the protected variable save as WeaponRange.
+*   @return this Shotguns WeaponRange.
+*/
 int Shotgun::GetRange()
 {
 	return this->WeaponRange;
 }
 
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+/** Default constructor for class Potion derived from base class Weapon, when an object is created from class Potion 
+*   it will be initialised with this list of inhereted protected variables that the author has allocated. To publicy
+*   access these protected variables other authors may use the Accessors below.
+*/
+
+Potion::Potion()
+{
+	this->loot = "Health Potion";
+	ID = 4;
+	value = 200;
+	this->Health = 10;		
+}
+
+Potion::~Potion(void)
+{
+
+}
+
+// ACCESSORS
+
+/** Accessor for class Potion in order to publicy access the protected string saved as loot.
+*   @return this Potions loot(name).
+*/
+string Potion::GetLoot()
+{
+	return this->loot;
+}
+
+/** Accessor for class Potion in order to publicy access the protected variable saved as ID.
+*   @return this Potions ID.
+*/
+int Potion::GetID()
+{
+	return this->ID;
+}
+
+/** Accessor for class Potion in order to publicy access the protected variable saved as Health.
+*   @return this Potions Health.
+*/
+int Potion::GetHealth()
+{
+	return this->Health;
+
+}
 
 
 //////////////////////////////////////////////////////////////////////////
